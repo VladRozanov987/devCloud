5d4b1081b7cfddf05f0e673a9dc611c033882f9c
Vlad Rozanov <vvladrozanov@gmail.com>
2022-04-04 16:40:44 +0300

    more sliders

--- assets/js/component/slider.js
+++ assets/js/component/slider.js
@@ -9,7 +9,6 @@ const slider = () => {
     });
 
     var swiperProcess = new Swiper(".mySwiperProcess", {
-        /*         direction: "horizontal", */
         pagination: {
             bulletActiveClass: 'process__swiper-bullet-is-active',
             bulletClass: 'process__swiper-bullet',
@@ -20,6 +19,44 @@ const slider = () => {
             },
         },
     });
-}
 
+    var techSlider = new Swiper(".technology__slider", {
+        slidesPerView: 9,
+        spaceBetween: 30,
+        loop: "true",
+        autoplay: {
+            delay: 1000,
+        },
+    });
+
+    var reverseTechSlider = new Swiper(".technology__slider__reverse", {
+        slidesPerView: 7,
+        spaceBetween: 30,
+        reverseDirection: "true",
+        loop: "true",
+        autoplay: {
+            delay: 1000,
+        },
+    });
+
+    var expirience__slider = new Swiper(".expirience__slider", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: "true",
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    var partners__slider = new Swiper(".partners__slider", {
+        slidesPerView: 6,
+        spaceBetween: 30,
+        reverseDirection: "true",
+        loop: "true",
+        autoplay: {
+            delay: 1000,
+        },
+    });
+}
 export default slider;
